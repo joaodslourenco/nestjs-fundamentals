c2d17be743cb41b15187faa8889a1c7e55d9521d
Joao Lourenco <joaodslourenco@gmail.com>
2023-06-19 14:42:56 -0300

    add verification to tags

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -4,13 +4,18 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { UpdateCourseDto } from './dto/update-course.dto';
+import { Tag } from './entities/tag.entity';
 
 @Injectable()
 export class CoursesService {
   constructor(
     @InjectRepository(Course)
     private readonly courseRepository: Repository<Course>,
+
+    @InjectRepository(Tag)
+    private readonly tagRepository: Repository<Tag>,
   ) {}
+
   findAll() {
     return this.courseRepository.find();
   }
@@ -24,15 +29,25 @@ export class CoursesService {
     return course;
   }
 
-  create(createCourseDto: CreateCourseDto) {
-    const course = this.courseRepository.create(createCourseDto);
+  async create(createCourseDto: CreateCourseDto) {
+    const tags = await Promise.all(
+      createCourseDto.tags.map((tag) => this.preloadTagByName(tag)),
+    );
+    const course = this.courseRepository.create({ ...createCourseDto, tags });
     return this.courseRepository.save(course);
   }
 
   async update(id: string, updateCourseDto: UpdateCourseDto) {
+    const tags =
+      updateCourseDto.tags &&
+      (await Promise.all(
+        updateCourseDto.tags.map((tag) => this.preloadTagByName(tag)),
+      ));
+
     const course = await this.courseRepository.preload({
       id: +id,
       ...updateCourseDto,
+      tags,
     });
 
     if (!course) {
@@ -48,5 +63,15 @@ export class CoursesService {
       throw new NotFoundException(`Course ID ${id} not found`);
     }
     return this.courseRepository.remove(course);
+  }
+
+  private async preloadTagByName(name: string): Promise<Tag> {
+    const tag = await this.tagRepository.findOne({ name });
+
+    if (tag) {
+      return tag;
+    }
+
+    return this.tagRepository.create({ name });
   }
 }
